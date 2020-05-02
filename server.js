2cb06a796e8455ea9255c4c8b4780665c1d68e54
pasi_ <pascal.ulmer@hs-furtwangen.de>
2020-05-02 16:43:46 +0200

    Felder auslesen/setzen und .HTML mit .ejs ersetzt

--- server.js
+++ server.js
@@ -1,61 +1,123 @@
+console.log("Server gestartet.")
+
 const express = require('express')
 const multer = require('multer')
-const path = require('path')
+const bodyParser = require('body-parser');
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
+var  uploadPic = multer ( {  dest : ' uploads / ' } )   
 
 var url = "mongodb://localhost:27017/"; //ToDo: Falsche Adresse
 
 //MongoDB
   //DB: CloudAppDatabase
   //Collection: MediaFiles
-function insertPost(){
+function insertPost(tit, sub, dat, ima, sto){
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("CloudAppDatabase");
+    var myobj = {title: tit, subtitle: sub, date: dat, image: ima, story: sto};  
+    dbo.collection("MediaFiles").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("New post inserted");
+      db.close();
+    });
+  });  
+}
+
+//MongoDB
+function getData(){
+  //ToDo: Daten von DB holen und auf Webseite setzen
     MongoClient.connect(url, function(err, db) {
+      
       if (err) throw err;
       var dbo = db.db("CloudAppDatabase");
-      var myobj = {title: "", subtitle: "", date: "", image: "", story: ""};  //ToDo: Daten noch übergeben
-      dbo.collection("MediaFiles").insertOne(myobj, function(err, res) {
-        if (err) throw err;
-        console.log("New post inserted");
-        db.close();
-      });
-    });  
-  }
-
-//Save image
-const storage = multer.diskStorage({
-    destination: './public/uploads/',
-    filename: function( req, file, cb){
-        cb(null,file.fieldname + "-" + Date.now() +
-        path.extname(file.originalname))
-    }
-});
+  
+      if (dbo.collection("MediaFiles").count(function (err, count){
+        if(!err && count ===0){
+          //if there are no pictures in db, then don't show title, image or subtitle on the homepage
+        }else{
+          dbo.collection("MediaFiles").findOne({}, function(err, result) {
+            if (err) throw err;
+            
+            var json = JSON.parse(result)
+            document.getElementById("title").innerText = json.title
+            document.getElementById("subtitle").innerText = json.subtitle
+            document.getElementById("image").src = json.source
+          
+            document.getElementById("date").innerText = json.date
+            document.getElementById("story").innerText = json.story
+  
+            db.close();
+          
+        });
+      }
+    }));
+  })
+}
 
 //Init upload
-const upload = multer({
-    storage:  storage
-}).single('myImage')
-
-console.log("Server gestartet.")
+const upload = multer({}).single('myImage')
 
 //Static folder
 app.use("/static", express.static('./static/'));
 
+//Start values
+var initTitle = "A"
+var initSubtitle = "B"
+var initDate = "C"
+var initStory = "D"
+
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-    res.sendFile('C:/Users/pasi_/Desktop/HTWG/1.Semester/Cloud Application/CloudAppLesson/static/index.html')
+    res.render('index', {title: initTitle, subtitle: initSubtitle, date: initDate, story: initStory})
 })
 
+//Set data to forms
+app.post('/vor', function(req, res) {
+
+  //ABFRAGE für DB
+
+  initTitle = "Nächster Title"
+  initSubtitle = "Nächster Subtitle"
+  initDate = "Nächstes Datum"
+  initStory = "Nächste Story"
+  res.render('index', {title: initTitle, subtitle: initSubtitle, date: initDate, story: initStory})
+})
+
+app.post('/zurueck', function(req, res) {
+
+  //ABFRAGE für DB
+
+  initTitle = "Vorheriger Title"
+  initSubtitle = "Vorheriger  Subtitle"
+  initDate = "Vorheriges Datum"
+  initStory = "Vorherige Story"
+  res.render('index', {title: initTitle, subtitle: initSubtitle, date: initDate, story: initStory})
+})
+
+app.use(bodyParser.urlencoded({ extended: true })); 
 //Button Upload
-app.post('/upload', (req, res) =>{
+app.post('/upload', uploadPic.single('myImage'),function (req, res){
+    
+    var newTitle = `${req.body.iTitle}`;
+    var newSubTitle = `${req.body.iSubtitle}`
+    var date = new Date()
+    var newDate = date.getHours() + ":" + date.getMinutes()
+    var newStory = `${req.body.iStory}`
+    
+    var newImage = `${req.file}`
+
+    console.log("DATA: " + newTitle + "" + newSubTitle + "" + newStory + "" + newImage.originalname )
+
     upload(req,res, (err) =>{
         if(err){
             res.send("Picture upload failed!")
         }
         else{
-            console.log(req.file)
-            res.send("Picture uploaded!")
             //Insert post into db
-            insertPost()
+            insertPost(newTitle, newSubTitle, newDate, newImage, newStory)
+            res.send("Picture uploaded!")
         }
     });
 })
--- static/client.js
+++ static/client.js
@@ -1,67 +1,6 @@
 console.log("Client-side code running");
 
-var newTitle
-var newSubtitle
-var newStory
- 
 //After load site
 function afterSiteLoad(){
-
-  //ToDo: Daten von DB holen und auf Webseite setzen
-
-  //Tassilos Code: noch nicht lauffähig...
-  /*
-   MongoClient.connect(url, function(err, db) {
-    
-    if (err) throw err;
-    var dbo = db.db("CloudAppDatabase");
-
-    if (dbo.collection("MediaFiles").count(function (err, count){
-      if(!err && count ===0){
-        //if there are no pictures in db, then don't show title, image or subtitle on the homepage
-      }else{
-        dbo.collection("MediaFiles").findOne({}, function(err, result) {
-          if (err) throw err;
-          
-          var json = JSON.parse(result)
-          document.getElementById("title").innerText = json.title
-          document.getElementById("subtitle").innerText = json.subtitle
-          document.getElementById("image").src = json.source
-        
-          document.getElementById("date").innerText = json.date
-          document.getElementById("story").innerText = json.story
-
-          db.close();
-        });
-      }
-    }));
-  */
-
-  document.getElementById("title").innerText = "Our last 15 days"
-  document.getElementById("subtitle").innerText = "America-Tour"
   document.getElementById("image").src = "https://pix10.agoda.net/geo/city/318/1_318_02.jpg?s=1920x822"
-
-  document.getElementById("date").innerText = "12.12.2019"
-  document.getElementById("story").innerText = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."     
-}
-
-//Save new post
-function getDataFromNewPost() {
-
-  var titleLable = document.getElementById("newTitle")
-  var subtitleLable = document.getElementById("newSubtitle")
-  var storyLable = document.getElementById("newStory")
-
-  newTitle = titleLable.value
-  newSubtitle = subtitleLable.value
-  newStory = storyLable.value
-
-  console.log("Title: " + newTitle)
-  console.log("Subtitle: " + newSubtitle)
-  console.log("Story: " + newStory)
-
-  titleLable.value = ""
-  subtitleLable.value = ""
-  storyLable.value = ""
-  
 }
