55ff65443bf53d1c05b48b4c3b969b674fa47cf6
danhau <daniel.hauber@web.de>
2020-05-05 19:01:01 +0200

    final

--- server.js
+++ server.js
@@ -1,23 +1,54 @@
 console.log("Server gestartet.")
 
+const fs = require('fs')
 const express = require('express')
 const multer = require('multer')
 const bodyParser = require('body-parser');
 const app = express()
-var  uploadPic = multer ( {  dest : ' uploads / ' } )   
+
+var dir = "./static"
+const dir2 = "./static/"
+
+if(!fs.existsSync(dir)){
+  fs.mkdir(dir, function(err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Directory images successfully created.")
+    }
+  })
+}else{
+  console.log("Directory images already exists.")
+}
+
+var name
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, dir)
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    name = uniqueSuffix + '.jpg'
+    cb(null, name)
+  }
+})
+
+var  uploadPic = multer ( {  storage : storage } )   
 var id=0;
 
 var initTitle = "A"
 var initSubtitle = "B"
 var initDate = "C"
 var initStory = "D"
+var pathIma = ""
 
 var number = 0
 var maxNumber
 
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://dbUser:sudo@cluster0-t4evc.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(url, { useNewUrlParser: true });
+//const url = "mongodb+srv://dbUser:sudo@cluster0-t4evc.mongodb.net/test?retryWrites=true&w=majority";
+const url = "mongodb://localhost"
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   //Only test, if db is available
@@ -32,7 +63,7 @@ function insertPost(tit, sub, dat, ima, sto){
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("CloudAppDatabase");
-    var myobj = {id: id, title: tit, subtitle: sub, date: dat, image: ima, story: sto};  
+    var myobj = {id: id, title: tit, subtitle: sub, date: dat, image: name, story: sto};  
     id =id+1;
     dbo.collection("MediaFiles").insertOne(myobj, function(err, res) {
         if (err) throw err;
@@ -56,6 +87,8 @@ function getData(i, callback){
       initSubtitle = result[i].subtitle
       initDate = result[i].date
       initStory = result[i].story
+      initName = result[i].image
+      pathIma = dir2 + initName
 
       db.close();
       callback()
@@ -85,7 +118,7 @@ app.get('/', (req, res) => {
 })
 
 function refresh(res){
-  res.render('index', {title: initTitle, subtitle: initSubtitle, date: initDate, story: initStory})
+  res.render('index', {title: initTitle, subtitle: initSubtitle, date: initDate, story: initStory, path: pathIma})
 }
 
 //Set data into forms
--- static/client.js
+++ static/client.js
@@ -1,6 +1,7 @@
 console.log("Client-side code running");
 
+
 //After load site
 function afterSiteLoad(){
-  document.getElementById("image").src = "https://pix10.agoda.net/geo/city/318/1_318_02.jpg?s=1920x822"
+  //document.getElementById("image").src = "https://pix10.agoda.net/geo/city/318/1_318_02.jpg?s=1920x822"
 }
